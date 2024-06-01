17a57d82917e4dd9f44a70dfe465413bec6fb636
nsb199 <singhneeraj199199@gmail.com>
2024-06-01 21:50:47 +0530

    initial commit

---
+++ src/movie.js
@@ -0,0 +1,166 @@
+export const movies = [
+    {
+        "image_url": "https://i.imgur.com/J0RY5CM.jpg",
+        "movie_name": "El Camino: A Breaking Bad Movie",
+        "release_year": 2019,
+        "duration": 122,
+        "genres": ["Action", "Drama"],
+        "description": "Fugitive Jesse Pinkman runs from his captors, the law, and his past.",
+        "made_by": "Vince Gilligan"
+    },
+    {
+        "image_url": "https://images-cdn.ubuy.co.in/634f9a12ec3ec2031f564253-schindler-39-s-list-movie-art.jpg",
+        "movie_name": "Schindler's List",
+        "release_year": 1993,
+        "duration": 195,
+        "genres": ["Biography", "Drama", "History"],
+        "description": "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.",
+        "made_by": "Steven Spielberg"
+    },
+    {
+        "image_url": "https://image.tmdb.org/t/p/original/jXbqfVHmvCikyTw2Lf5OhKyt9Ym.jpg",
+        "movie_name": "The Social Network",
+        "release_year": 2010,
+        "duration": 120,
+        "genres": ["Biography", "Drama"],
+        "description": "As Harvard student Mark Zuckerberg creates the social networking site that would become known as Facebook, he is sued by the twins who claimed he stole their idea, and by the co-founder who was later squeezed out of the business.",
+        "made_by": "David Fincher"
+    },
+    
+    {
+        "image_url": "https://image.tmdb.org/t/p/original/7fn624j5lj3xTme2SgiLCeuedmO.jpg",
+        "movie_name": "Whiplash",
+        "release_year": 2014,
+        "duration": 107,
+        "genres": ["Drama", "Music"],
+        "description": "A promising young drummer enrolls at a cut-throat music conservatory where his dreams of greatness are mentored by an instructor who will stop at nothing to realize a student's potential.",
+        "made_by": "Damien Chazelle"
+    },
+    {
+        "image_url": "https://i.etsystatic.com/23402008/r/il/8b111e/2375948959/il_570xN.2375948959_6kw0.jpg",
+        "movie_name": "The Departed",
+        "release_year": 2006,
+        "duration": 151,
+        "genres": ["Crime", "Drama", "Thriller"],
+        "description": "An undercover cop and a mole in the police attempt to identify each other while infiltrating an Irish gang in South Boston.",
+        "made_by": "Martin Scorsese"
+    },
+    {
+        "image_url": "https://lh4.googleusercontent.com/proxy/gx8zohkhtjTAre8WcCB8aQJajDrXl-JSrd8dY5zZ8bclKWDUe2-mKaTKM1Y1p_Sluiuu1c9k-uvBbCSye8lfAN7yVutu",
+        "movie_name": "The Prestige",
+        "release_year": 2006,
+        "duration": 130,
+        "genres": ["Drama", "Mystery", "Sci-Fi"],
+        "description": "After a tragic accident, two stage magicians engage in a battle to create the ultimate illusion while sacrificing everything they have to outwit each other.",
+        "made_by": "Christopher Nolan"
+    },
+    {
+        "image_url": "https://m.media-amazon.com/images/I/713kiC-8JhL._AC_UF1000,1000_QL80_.jpg",
+        "movie_name": "The Grand Budapest Hotel",
+        "release_year": 2014,
+        "duration": 100,
+        "genres": ["Adventure", "Comedy", "Crime"],
+        "description": "A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel's glorious years under an exceptional concierge.",
+        "made_by": "Wes Anderson"
+    },
+    {
+        "image_url": "https://i.pinimg.com/736x/b4/43/ac/b443ac179682a3de6e45bff8962c337a.jpg",
+        "movie_name": "Fight Club",
+        "release_year": 1999,
+        "duration": 139,
+        "genres": ["Drama"],
+        "description": "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into something much, much more.",
+        "made_by": "David Fincher"
+    },
+    {
+        "image_url": "https://fiu-original.b-cdn.net/fontsinuse.com/use-images/168/168470/168470.jpeg?filename=12322079%5D&call=url%5Bfile:product.chain%5D",
+        "movie_name": "The Green Mile",
+        "release_year": 1999,
+        "duration": 189,
+        "genres": ["Crime", "Drama", "Fantasy"],
+        "description": "The lives of guards on Death Row are affected by one of their charges: a black man accused of child murder and rape, yet who has a mysterious gift.",
+        "made_by": "Frank Darabont"
+    },
+    {
+        "image_url": "https://i.pinimg.com/564x/dd/c6/e4/ddc6e435721b8a7e62c4f9bb128ae83c.jpg",
+        "movie_name": "Goodfellas",
+        "release_year": 1990,
+        "duration": 146,
+        "genres": ["Biography", "Crime", "Drama"],
+        "description": "The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito.",
+        "made_by": "Martin Scorsese"
+    },
+    {
+        "image_url": "https://i.pinimg.com/564x/2e/a8/41/2ea841b73698751f65722fe31efaa386.jpg",
+        "movie_name": "Breaking Bad",
+        "release_year": 2008,
+        "duration": 49,
+        "genres": ["Crime", "Drama", "Thriller"],
+        "description": "A high school chemistry teacher turned methamphetamine manufacturer partners with a former student to secure his family's future.",
+        "made_by": "Vince Gilligan"
+    },
+    {
+        "image_url": "https://i.pinimg.com/564x/02/7c/aa/027caa846ac8f9025de1c3ccf5c98d6f.jpg",
+        "movie_name": "Game of Thrones",
+        "release_year": 2011,
+        "duration": 57,
+        "genres": ["Action", "Adventure", "Drama"],
+        "description": "Nine noble families fight for control over the lands of Westeros, while an ancient enemy returns after being dormant for millennia.",
+        "made_by": "David Benioff, D.B. Weiss"
+    },
+    {
+        "image_url": "https://i.pinimg.com/564x/84/a6/8c/84a68cc25e114550a227b57ca00614ce.jpg",
+        "movie_name": "Stranger Things",
+        "release_year": 2016,
+        "duration": 51,
+        "genres": ["Drama", "Fantasy", "Horror"],
+        "description": "When a young boy disappears, his mother, a police chief, and his friends must confront terrifying supernatural forces in order to get him back.",
+        "made_by": "Matt Duffer, Ross Duffer"
+    },
+    {
+        "image_url": "https://i.pinimg.com/564x/8e/1a/1a/8e1a1a3913fd44a2188dac98602a7a81.jpg",
+        "movie_name": "The Witcher",
+        "release_year": 2019,
+        "duration": 60,
+        "genres": ["Action", "Adventure", "Drama"],
+        "description": "Geralt of Rivia, a mutated monster-hunter for hire, journeys toward his destiny in a turbulent world where people often prove more wicked than beasts.",
+        "made_by": "Lauren Schmidt Hissrich"
+    },
+    {
+        "image_url": "https://i.pinimg.com/564x/49/7b/8b/497b8bdd152ea05fce5654a0b8c799f7.jpg",
+        "movie_name": "The Mandalorian",
+        "release_year": 2019,
+        "duration": 40,
+        "genres": ["Action", "Adventure", "Fantasy"],
+        "description": "The travels of a lone bounty hunter in the outer reaches of the galaxy, far from the authority of the New Republic.",
+        "made_by": "Jon Favreau"
+    },
+    {
+        "image_url": "https://i.pinimg.com/564x/b4/b8/b8/b4b8b8ea0344ec84583256d2821dde60.jpg",
+        "movie_name": "The Crown",
+        "release_year": 2016,
+        "duration": 58,
+        "genres": ["Biography", "Drama", "History"],
+        "description": "Follows the political rivalries and romance of Queen Elizabeth II's reign and the events that shaped the second half of the 20th century.",
+        "made_by": "Peter Morgan"
+    },
+    {
+        "image_url": "https://m.media-amazon.com/images/I/71715eBi1sL._AC_UF894,1000_QL80_.jpg",
+        "movie_name": "The Shawshank Redemption",
+        "release_year": 1994,
+        "duration": 142,
+        "genres": ["Drama"],
+        "description": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+        "made_by": "Frank Darabont"
+    },
+    {
+        "image_url": "https://m.media-amazon.com/images/I/81C9FT0-8CL._AC_UF1000,1000_QL80_.jpg",
+        "movie_name": "The Godfather",
+        "release_year": 1972,
+        "duration": 175,
+        "genres": ["Crime", "Drama"],
+        "description": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+        "made_by": "Francis Ford Coppola"
+    }
+
+]
